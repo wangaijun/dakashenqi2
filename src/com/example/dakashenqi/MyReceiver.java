8b18944d0ad961add5b3d097d13bbb464b0256ca
wangaijun <hw_wangaijun@163.com>
2013-12-27 17:52:18 +0800

    12月27日

--- src/com/example/dakashenqi/MyReceiver.java
+++ src/com/example/dakashenqi/MyReceiver.java
@@ -10,7 +10,7 @@ import android.content.Intent;
  */
 public class MyReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
-        Intent intent1 = new Intent(context,Service.class);
+        Intent intent1 = new Intent(context,MyService.class);
         context.startService(intent1);
     }
 }
